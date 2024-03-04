6ec22c4db3a2e32262173672605113d7b5b3c105
BethTawiah <elizabeth.tawiah@amalitech.org>
2024-03-04 11:12:48 +0000

    HorizontalSection1 set

--- careplan/src/HorizontalSection2.js
+++ careplan/src/HorizontalSection2.js
@@ -3,13 +3,30 @@ import './HorizontalSection2.css'; // Import CSS file for HorizontalSection2
 
 function HorizontalSection2() {
   return (
-    <div className="horizontal-part bigger-size">
-     <div className="header2">
-        <h2 className="header2-title">Care Plan</h2>
-      </div>
-      {/* Content for the first horizontal part */}
+    <><div className="content-column sidebar">
+      <div className="container sidebar">
+        <div className="title-bar sidebar">
+          <div className="title-bar-block">
+            <div className="title-bar-block">
+              <div data-w-id="4415eedb-f0b9-bb57-e355-13ea7e9cdd36" className="navbar-menu-button">
+                <img src="images/menu-03.svg" loading="lazy" alt="" className="navbar-menu-icon" />
+              </div>
+              <a href="set-care-plan.html" className="nav-icon back w-inline-block"></a>
+              <div className="title-grid-block">
+                <div className="nav-title-h1">Care Plan</div>
+              </div>
+
+        
+    
+    
     </div>
-  );
+            </div>
+          </div>
+        </div>
+        </div>
+      
+        </>
+  )
 }
 
 export default HorizontalSection2;
